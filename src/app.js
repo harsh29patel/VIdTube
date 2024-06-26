a0d27d9f85fc771b56aca8e29a0e569d3d8ea44a
harsh29patel <harshpatel290703@gmail.com>
2024-06-26 12:02:11 +0530

    DataBase connection

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from "cors"
+
+const app = express()
+
+app.use(
+    cors({                  //CORS Middleware
+        origin: process.env.CORS_ORIGIN,
+        credentials: true
+    })
+)  
+
+app.use(express.json({limit:"16kb"}))   // express Middleware
+app.use(express.urlencoded({extended:true , limit:"16kb"})) // urlencoded because in url when space is there it represents %20 
+app.use(express.static("public")) // in public folder any image or anything to be served i go to static.folder
+
+
+
+
+export {app} 
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import { app } from "./app.js";
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path:"./.env"
+})
+
+const PORT =  process.env.PORT || 6000
+
+connectDB()
+.then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Server is running on port ${PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongo DB connection error" , err);
+})
